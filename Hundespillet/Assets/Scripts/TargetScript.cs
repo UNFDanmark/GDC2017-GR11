a9c0868c8de706da758f576eff68dc84a3dcc7fa
WIN\AIT-UNF-GDC - Programmering grp. 11 <gamedc2017@gmail.com>
2017-07-27 13:09:24 +0200

    Tilføjet textures til hund og lavede svævende bikinier.

--- Hundespillet/Assets/Scripts/TargetScript.cs
+++ Hundespillet/Assets/Scripts/TargetScript.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class TargetScript : MonoBehaviour {
 
+	public GameObject target;
 	public float rotationSpeed = 20;
+	public float amplitude = 50;
+	public float frequency = 2;
+	public float phase =0;
+	public float targetStartingY = 1.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +18,8 @@ public class TargetScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (0, rotationSpeed * Time.deltaTime, 0, Space.World);
+		target.transform.position = new Vector3 (target.transform.position.x, amplitude*Mathf.Cos (frequency*Time.timeSinceLevelLoad+phase)+targetStartingY, target.transform.position.z);
+
 	}
+
 }
