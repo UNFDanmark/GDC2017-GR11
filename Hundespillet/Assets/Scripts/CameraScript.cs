473d7cd69916be14c752a541835e70e538e980a6
WIN\AIT-UNF-GDC - Programmering grp. 11 <gamedc2017@gmail.com>
2017-07-27 15:11:42 +0200

    Lavede kamerabevægelse

---
+++ Hundespillet/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	public float cameraMoveModifier = 0.1f;
+	public GameObject dogg;
+
+	// Use this for initialization
+	void Awake(){
+		dogg = GameObject.Find ("Dogg");
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 ((dogg.transform.position.x*cameraMoveModifier)-transform.position.x, 0, 0));
+	}
+}
